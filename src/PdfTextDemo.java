0f63931c0372d840a68a45ec267522a2d40be5aa
anurag singhal <ansinghal@gmail.com>
2015-05-25 14:43:37 -0400

    2nd commit-save in a text file

--- src/PdfTextDemo.java
+++ src/PdfTextDemo.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.*;
 
 
 public class PdfTextDemo {
@@ -6,8 +7,19 @@ public class PdfTextDemo {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		 PDFManager pdfManager = new PDFManager();
-		pdfManager.setFilePath("C:\\sample.pdf");
-	       System.out.println(pdfManager.ToText());      
+		pdfManager.setFilePath("C:\\test.pdf");
+	            
+	       
+	       File file = new File("c:\\newfile.txt");
+	       file.createNewFile();
+	       
+	       FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(pdfManager.ToText());
+			bw.close();
+			System.out.println("Done");
+			
+			 
 	       
 	    
 	}
