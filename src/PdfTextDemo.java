db10be48b6baa404a1c60ce160abd73064d1f5cc
aditya bhatia <aditya.bhatia52@gmail.com>
2015-05-26 15:16:21 +0530

    file path changed for input and output files to desktop

--- src/PdfTextDemo.java
+++ src/PdfTextDemo.java
@@ -6,18 +6,17 @@ public class PdfTextDemo {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		 PDFManager pdfManager = new PDFManager();
-		pdfManager.setFilePath("C:\\test.pdf");
-	            
-	       
-	       File file = new File("c:\\newfile.txt");
+		   PDFManager pdfManager = new PDFManager();
+		   pdfManager.setFilePath("C:\\Users\\IBM_ADMIN\\Desktop\\Project\\test.pdf");
+	                  
+	       File file = new File("C:\\Users\\IBM_ADMIN\\Desktop\\Project\\output\\t1.txt");
 	       file.createNewFile();
 	       
 	       FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(pdfManager.ToText());
-			bw.close();
-			System.out.println("Done");
+	       BufferedWriter bw = new BufferedWriter(fw);
+	       bw.write(pdfManager.ToText());
+	       bw.close();
+	       System.out.println("Done");
 			
 			 
 	       
