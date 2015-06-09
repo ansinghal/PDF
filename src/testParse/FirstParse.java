045d11b3b4881a049dae86d0210a8379f6578821
aditya bhatia <aditya.bhatia52@gmail.com>
2015-06-09 14:53:01 +0530

    test for the parse function

---
+++ src/testParse/FirstParse.java
@@ -0,0 +1,122 @@
+package testParse;
+/*
+ * This class contains the function that reads the pdf file and converts it into a text file that:
+ * i)has trimmed all the irrelevant information.
+ * ii)corrects and replaces the text in files($ for :) so that : can be used as a delimiter.
+ * 
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+public class FirstParse {
+	private PDFParser parser;
+	   private PDFTextStripper pdfStripper;
+	   private PDDocument pdDoc ;
+	   private COSDocument cosDoc ;
+	  
+	   private String Text ;
+	   private String filePath;
+	   private File file;
+	 
+	    public FirstParse() 
+	    { } 
+	        
+	    
+	   public String ToText() throws IOException
+	   {
+	       this.pdfStripper = null;
+	       this.pdDoc = null;
+	       this.cosDoc = null;
+	      
+	       file = new File(filePath);
+	       parser = new PDFParser(new FileInputStream(file));
+	      
+	       parser.parse();
+	       cosDoc = parser.getDocument();
+	       pdfStripper = new PDFTextStripper();
+	       pdDoc = new PDDocument(cosDoc);
+	       // to read all the text:
+	       pdfStripper.setEndPage(pdDoc.getNumberOfPages());
+	       Text = pdfStripper.getText(pdDoc);
+	       //to get total number of price quotes in the given pdf:
+	       int count = 0;
+	       int indices[] = new int[100];
+	       int j = Text.indexOf("New Subtotals");
+	       if(j==-1)
+	       {
+	    	   System.exit(-1);
+	       }
+	      int c=1;
+	      indices[0] = 0;
+	       while(j >= 0) {
+	    	    //System.out.println("index of new subtotal = "+j);
+	    	    indices[c]=j;
+	    	    j = Text.indexOf("New Subtotals", j+1);
+	            c++;
+	            count ++;
+	       }
+	       System.out.println("total number of price quotes is:"+count);
+	       //for not including the extra text at the start and end of the file:
+	       int description = Text.indexOf("Description");
+	       if(description == -1)
+	       {
+	    	   System.out.println("Keyword Description is not found");
+	       }
+	       int start = Text.indexOf("Server");
+	       if(start == -1)
+	       {
+	    	   start  = Text.indexOf("Computing Instance");
+	       }
+	       indices[0]=start;
+	      // int end = Text.lastIndexOf("New Subtotals");
+	       j=0;
+	       String Text1;
+	       String Finish = null;
+	       for(j=0;j<count-1;j++)
+	    	   {
+	    	   		System.out.println("\n\nend="+indices[j]);
+	    	   		if(j!=0)
+	    	   			start = indices[j]+45;
+	    	   		start = indices[j];
+	    	   		int ends = indices[j+1];
+	    	   		System.out.println("start="+start+"\nends="+ends);
+	    	   		Text1 = Text.substring(start,ends);
+	    	   		System.out.println(Text1);
+	    	   
+	       //quantity row doesn't have proper delimiters,inserting $ after every number
+	       //except the last:
+	       int index = Text1.indexOf("Quantity");
+	       //length of quantity + 1 blank space
+	       index = index + 9;
+	       int i;//loop index
+	       //converting to stringbuilder to use the insert function
+	       StringBuilder t = new StringBuilder(Text1);
+	       for(i=0;i<4;i++)
+	       {
+	    	   t.insert(index, " $");
+	    	   index = index + 4;
+	       }
+	    	   
+	       //replacing $ by :
+	       Text1 = t.toString();//converting back to a string
+	       Text1 = Text1.replace("$", " : ");
+	       Finish= Finish + (Text1);
+	    	   }
+	       
+	       
+	       cosDoc.close();
+	       return Finish;
+	   }
+	 
+	    public void setFilePath(String filePath) {
+	        this.filePath = filePath;
+	    }
+		
+	  
+	}
