a952f27c4dae94bf4886f81b2397f5322d088170
Ronit <ronitsharma294@outlook.com>
2015-06-19 11:06:34 +0530

    Front End 2

--- src/Me.java
+++ src/Me.java
@@ -15,17 +15,24 @@ import xmlGenerator.XmlGenerator;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
+
 import java.awt.SystemColor;
+
 import javax.swing.JPopupMenu;
+
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JToolBar;
+import javax.swing.JPanel;
 
 
 public class Me {
@@ -68,7 +75,7 @@ public class Me {
 	 */
 	public void initialize() {
 		frame = new JFrame();
-		frame.setBackground(new Color(112, 128, 144));
+		frame.setBackground(new Color(204, 0, 51));
 		frame.setBounds(300, 250, 493, 319);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -87,19 +94,15 @@ public class Me {
 		frame.getContentPane().add(textArea);
 		
 		JLabel lblNewLabel_1 = new JLabel("Quote Converter");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1.setFont(new Font("Franklin Gothic Medium", Font.BOLD, 24));
 		lblNewLabel_1.setForeground(Color.BLUE);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(158, 6, 136, 44);
+		lblNewLabel_1.setBounds(158, 6, 191, 44);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JButton btnRun = new JButton("Run");
 		btnRun.setBounds(50, 224, 89, 34);
 		frame.getContentPane().add(btnRun);
-		
-		JLabel label = new JLabel("");
-		label.setBounds(25, 224, 353, 14);
-		frame.getContentPane().add(label);
 		
 		JLabel lblOutputPath = new JLabel("Output Path");
 		lblOutputPath.setBounds(10, 183, 67, 14);
@@ -108,29 +111,6 @@ public class Me {
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(81, 172, 386, 29);
 		frame.getContentPane().add(textPane);
-		
-		JButton btnConfigFile = new JButton("Config. File");
-		btnConfigFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnConfigFile.setBounds(275, 224, 89, 34);
-		frame.getContentPane().add(btnConfigFile);
-		
-		JPopupMenu popupMenu = new JPopupMenu();
-		addPopup(btnConfigFile, popupMenu);
-		
-		JButton btnViewConfigFile = new JButton("View Config File");
-		popupMenu.add(btnViewConfigFile);
-		
-		JButton btnEditEntries = new JButton("Edit Entries");
-		popupMenu.add(btnEditEntries);
-		
-		JButton btnDeleteEntries = new JButton("Delete Entries");
-		popupMenu.add(btnDeleteEntries);
-		
-		JButton btnAddEntries = new JButton("Add Entries");
-		popupMenu.add(btnAddEntries);
 		
 		JLabel lblEnterNumberOf = new JLabel("Enter No. Of Files");
 		lblEnterNumberOf.setBounds(10, 54, 114, 34);
@@ -141,10 +121,28 @@ public class Me {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setBounds(149, 212, 318, 58);
+		frame.getContentPane().add(toolBar);
+		
+		JLabel lblConfigfile = new JLabel("Config.File");
+		toolBar.add(lblConfigfile);
+		
+		JButton btnView = new JButton("View");
+		toolBar.add(btnView);
+		
+		JButton btnAdd = new JButton("Add");
+		toolBar.add(btnAdd);
+		
+		JButton btnEdit = new JButton("Edit");
+		toolBar.add(btnEdit);
+		
+		JButton btnDelete = new JButton("Delete");
+		toolBar.add(btnDelete);
+		
 	      btnRun.addActionListener(new ActionListener() {
 	         public void actionPerformed(ActionEvent e) {     
 	            data = textArea.getText();
-	            label.setText(data);
 	            FirstParse pdfManager = new FirstParse();
 	            pdfManager.setFilePath(data);
 	            File file = new File("C:\\Users\\IBM_ADMIN\\Desktop\\Project\\output\\test.txt");
@@ -183,23 +181,5 @@ public class Me {
 	      }) ;
 	}
 	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-			
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
 	}
 }
